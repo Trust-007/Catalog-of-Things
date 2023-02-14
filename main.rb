b42276d9a4f7af80aef4c3e6b253e3c1c54fb135
kennankole <kennankole@gmail.com>
2023-02-14 17:44:07 +0300

    Created the catalog menu.

--- main.rb
+++ main.rb
@@ -1,5 +1,43 @@
+require('./menu')
+
 def main
+  # rubocop:disable Metrics/MethodLength
   puts 'Welcome to my Catalog of things'
+  flag = true
+  while flag
+    puts menu
+    choice = gets.chomp
+    case choice
+    when '1'
+      puts 'Book list coming soon'
+    when '2'
+      puts 'Music albums coming soon'
+    when '3'
+      puts 'Movies coming soon'
+    when '4'
+      puts 'Games coming soon'
+    when '5'
+      puts 'All genres coming soon'
+    when '6'
+      puts 'All labels comming soon'
+    when '7'
+      puts 'All others coming soon'
+    when '8'
+      puts 'All sources will be here'
+    when '9'
+      puts 'You cannot add a book now'
+    when '10'
+      puts 'Music system under maientance'
+    when '11'
+      puts 'Movie system coming soon'
+    when '12'
+      puts 'Game section is out of service'
+    when '13'
+      puts 'Thank for you using this app'
+      flag = false
+    end
+  end
+  # rubocop:enable Metrics/MethodLength
 end
 
 main
---
+++ menu.rb
@@ -0,0 +1,16 @@
+def menu
+  "\nKindly choose a service:\n" \
+    "1 - List all books \n" \
+    "2 - List all music albums \n" \
+    "3 - List all movies \n" \
+    "4 - List of games \n" \
+    "5 - List all genres \n" \
+    "6 - List all labels \n" \
+    "7 - List all authors \n" \
+    "8 - List all sources \n" \
+    "9 - Add a book \n" \
+    "10 - Add a music album \n" \
+    "11 - Add a movie \n" \
+    "12 - Add a game \n" \
+    "13 - Exit \n"
+end
