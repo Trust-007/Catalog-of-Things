df320cea709d243cc449864924e55592012f4f90
Trust7 <simeontrust7@gmail.com>
2023-02-16 14:40:49 +0100

    fixed linters

--- spec/books_spec.rb
+++ spec/books_spec.rb
@@ -3,8 +3,8 @@ require('./book')
 
 describe Book do
   context 'Testing the Book class creation' do
-    let(:book1) { Book.new( Date.iso8601('2023-10-01'), 'New York Times', 'Bad') }
-    let(:book2) { Book.new( Date.iso8601('2022-10-04'), 'Nairobi Book club', 'Good') }
+    let(:book1) { Book.new(Date.iso8601('2023-10-01'), 'New York Times', 'Bad') }
+    let(:book2) { Book.new(Date.iso8601('2022-10-04'), 'Nairobi Book club', 'Good') }
 
     it 'Returns publisher' do
       expect(book1.publisher).to eq('New York Times')
@@ -24,12 +24,12 @@ describe Book do
 
   context "Testing the 'can_be_archived_method' " do
     it 'Returns true' do
-      book = Book.new( Date.iso8601('2023-10-01'), 'Nrb Publishers', 'Bad')
+      book = Book.new(Date.iso8601('2023-10-01'), 'Nrb Publishers', 'Bad')
       expect(book.can_be_archived?).to eq(true)
     end
 
     it 'Returns false' do
-      book = Book.new( Date.iso8601('2023-10-01'), 'Nairobi Publishers', 'Good')
+      book = Book.new(Date.iso8601('2023-10-01'), 'Nairobi Publishers', 'Good')
       expect(book.can_be_archived?).to eq(false)
     end
   end
